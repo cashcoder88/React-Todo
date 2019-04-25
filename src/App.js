b4aa7a66b15739715b732a7823fe79970401da6a
cashcoder88 <cashglobe@gmail.com>
2019-04-24 18:39:55 -0600

    added for switch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-
-import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <TodoForm />
+        <h2>Welcome to your Todo App!</h2>
       </div>
     );
   }
 }
 
 export default App;
+
