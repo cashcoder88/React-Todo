248e2257771c7860aaa9b52ce53732d6a709d0d2
cashcoder88 <cashglobe@gmail.com>
2019-04-25 14:17:10 -0600

    TodoForm somewhat completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+/*- `<App />` will hold all the data needed for this project. It will
+ also be the container for your Todo Components.
+  - All of your application data will be stored here on `<App />`.
+  - All of your `handler` functions should live here on `<App />`.*/
+
 import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todoArray: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+
   render() {
     return (
       <div>
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,7 +1,24 @@
+// - `<TodoForm>` will hold your input field and your `Add Todo` and `Clear Completed` 
+// buttons.
+//   - Your input field should take in user input, and allow a user to press `Enter` 
+//   or click on the `Submit Button` to add a todo to your list.
+//   - Once a todo is submitted, the Todo List should re-render and show the added todo. *
+
 import React from 'react';
 
-function TodoForm {
-
+function TodoForm(props) {
+  <div>
+      <form>
+        <input 
+            placeholder="Add Todo"
+            type="text"
+            value={props.value}
+            onChange={props.handleChanges}
+            name="name"
+        />
+        <button onClick={props.handleAddTodo}>Add</button>
+      </form>
+  </div>  
 }
 export default TodoForm;
 
