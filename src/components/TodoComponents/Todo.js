738202b6fb3e293c0575627e3190e09a9b12d7bf
cashcoder88 <cashglobe@gmail.com>
2019-04-25 14:23:58 -0600

    Working Todo/TodoList

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,6 +1,14 @@
+
+// - `<Todo />` is a component that takes in the `todo` data and displays 
+// the task to the screen.
+
 import React from 'react';
 
-function Todo {
-
+function Todo(props) {
+    return(
+        <div>
+            {props.todo.task}
+        </div>
+    )
 }
 export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,8 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+// - `<TodoList />` receives your Todos array and iterates over the list 
+// generating a new `<Todo />` for each element in the array.
+
 import React from 'react';
+import Todo from './Todo';
 
-function TodoList {
-
+function TodoList(props) {
+    return (
+        <div>
+            {props.todos.map(todo => (
+               <Todo /> 
+            ))}
+        </div>
+    )
 }
 export default TodoList;
